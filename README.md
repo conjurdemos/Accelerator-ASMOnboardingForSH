c4475cbb47a8068932f64417ced8b629abc1efbf
Joseph Hunt <jodyhunt@hotmail.com>
2024-01-04 17:07:54 -0600

    refactored - checkpoint

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
-# secretshub-onboarding-lambda
+# Accelerator-ASMOnboardingForSH
+###Onboarding of AWS RDS secrets from AWS Secrets Manager to CyberArk Vaults
+#### with rotation by CPM and syncing with Secrets Hub
+
+## Goals:
+- Show how to enable CyberArk Privilege Cloud to manage AWS RDS secrets that were first created in AWS Secrets Manager.
+- Automate onboarding for AWS Relational Database Service (RDS) secrets in AWS Secrets Manager (ASM) to a designated safe in a CyberArk Privilege Cloud vault.
+- Enable CPM to rotate secret and SecretsHub to sync secrets back to the same ASM secret.
+
+## Prerequisites
+ - Admin access to an AWS account with admin access for Lambda & AWS Secrets Manager services.
+ - Admin access to a NON-PRODUCTION Cyberark Identity tenant
+ - Admin access to a NON-PRODUCTION CyberArk Privilege Cloud tenant
+ - Admin access to a NON-PRODUCTION CyberArk Secrets Hub tenant
+ - A Safe in Privilege Cloud with the above service user as member with Access and Account Management permissions, and the 'Conjur Sync' user as member with Access and Workflow permissions.
+ - A demo host - a MacOS or Linux VM environment with Docker or Podman installedand IPV4 network access to the CyberArk tenants.
+ - Make sure all scripts are executable. Run: chmod -R +x *.sh
+
+## STEP ONE: Manual Setup
+   - https://aws.amazon.com/blogs/security/how-to-connect-to-aws-secrets-manager-service-within-a-virtual-private-cloud/
+   - https://docs.aws.amazon.com/lambda/latest/dg/configuration-vpc.html
+ - create lambda, upload zipfile
+ - increase lambda default timeout in Configuration->General Configuration
+ - create ASM secret for CyberArk service account:
+   - key: subdomain
+     value: tenant subdomain prefix
+   - key: username
+     value: Oauth2 service user username
+   - key: password
+     value: Oauth2 service user password
+ - create env var named PrivilegeCloudSecret with name of ASM secret.
+
+ASM secrets must have these tags:
+CyberArk Safe
+CyberArk Platform
+CyberArk Account
+Sourced by CyberArk
+
+   - SAFE_NAME
+     - Must be the name of an existing safe.
+     - Name is case sensitive and should not contain spaces.
+     - The CyberArk admin service user must be a member with at least Access and Account Management permissions.
+     - 'Conjur Sync' must be a member with Access and Workflow permissions.
+
+## STEP TWO: Start Script
+
+## Use-Case 1 - Ansible plugin
+
+## Use-Case 3 - Password rotation
+
+### Sequence Diagram:
+![Ansible Workflow](https://github.com/conjurdemos/Accelerator-Ansible/blob/main/Ansible-Workflow.png?raw=true)
+
+## Description of Demo
+
+## Description of Repo Contents
--- lambda_function.py
+++ lambda_function.py
@@ -7,13 +7,20 @@ import requests
 import boto3
 from botocore.exceptions import ClientError
 
+# CONSTANTS
+
+DEBUG=False
+
+# environment variable containing name of admin secret in ASM
+PCLOUD_ADMIN_SECRET_ENV_VAR="PrivilegeCloudSecret"
+
+#-------------------------------------------
 def urlify(s):
     """URL encodes a given string"""
     return urllib.parse.quote(s)
 
-
-def lambda_handler(event, context):
-    """Lambda function handler"""
+#-------------------------------------------
+def prologOut(event, context):
     # Print the event to see its structure (useful for debugging)
     #print("Received event: " + json.dumps(event, indent=2))
 
@@ -24,119 +31,116 @@ def lambda_handler(event, context):
     print("Lambda Request ID: " + context.aws_request_id)
     print("Lambda function memory limits in MB: " + context.memory_limit_in_mb)
 
-    try:
-        # Extract the secret ID from the CloudWatch Event
-        secret_id = event['detail']['requestParameters']['name']
+#-------------------------------------------
+def validateSecretTags(secrets_manager_client, secret_id):
+  # Validate secret is tagged w/ 'Sourced by CyberArk' - if not, flag as not found
+  # returns dictionary of secret tags, status_code & response_body message
 
-        # Initialize a client for AWS Secrets Manager
-        secrets_manager_client = boto3.client('secretsmanager')
-
-        # Retrieve the tags of the secret
+        status_code = 200
+        response_body = json.dumps(f"Secret {secret_id} sourced by CyberArk")
+        
+        # Retrieve tags of the secret
         response = secrets_manager_client.describe_secret(SecretId=secret_id)
         tags = response.get('Tags', [])
         tags_dict = {tag['Key']: tag['Value'] for tag in tags}
 
-        # Check for Sourced by CyberArk tag
         if 'Sourced by CyberArk' not in tags_dict:
-            return {
-                'statusCode': 200,
-                'body': json.dumps(f"Secret {secret_id} not sourced by CyberArk")
-            }
+            status_code = 404
+            response_body = f"Secret {secret_id} not sourced by CyberArk"
 
-        # Retrieve the secret value of PrivilegeCloudSecret
-        pcloud_secret_id = os.environ.get("PrivilegeCloudSecret")
-        try:
-            pcloud_response = secrets_manager_client.get_secret_value(SecretId=pcloud_secret_id)
-        except ClientError as e:
-            if e.response['Error']['Code'] == 'ResourceNotFoundException':
-                print("The requested secret " + pcloud_secret_id + " was not found")
-            elif e.response['Error']['Code'] == 'InvalidRequestException':
-                print("The request was invalid due to:", e)
-            elif e.response['Error']['Code'] == 'InvalidParameterException':
-                print("The request had invalid params:", e)
-            elif e.response['Error']['Code'] == 'DecryptionFailure':
-                print("The requested secret can't be decrypted using the provided KMS key:", e)
-            elif e.response['Error']['Code'] == 'InternalServiceError':
-                print("An error occurred on service side:", e)
-        else:
-            pcloud_secret_value = pcloud_response.get('SecretString', None)
-            pcloud_secret_dict = json.loads(pcloud_secret_value)
+        if DEBUG:
+           print("validateSecretTags()================") 
+           print(f"\tsecret_id: {secret_id}")
+           print(f"\tstatus_code: {status_code}\n\tresponse: {response_body}")
 
-        # Retrieve the secret value of the secret
+        return tags_dict, status_code, response_body
+
+#-------------------------------------------
+def getAsmSecretValue(secrets_manager_client, secret_id):
+  # uses client & secret_id to retrieve value of secret
+  # returns secret_value dictionary, status_code & message for response_body
+
+        secret_value = {}
+        status_code = 200
+        response_body = "Secret retrieved successfully."
+
         try:
             response = secrets_manager_client.get_secret_value(SecretId=secret_id)
         except ClientError as e:
             if e.response['Error']['Code'] == 'ResourceNotFoundException':
-                print("The requested secret " + secret_name + " was not found")
+                status_code = 404
+                response_body = json.dumps(f"The requested secret {secret_id} was not found.")
+            elif e.response['Error']['Code'] == 'AccessDeniedException':
+                status_code = 403
+                response_body = json.dumps(f"Access to the requested secret {secret_id} was denied.")
             elif e.response['Error']['Code'] == 'InvalidRequestException':
-                print("The request was invalid due to:", e)
+                status_code = 500 
+                response_body = json.dumps(f"The request was invalid due to: {e}")
             elif e.response['Error']['Code'] == 'InvalidParameterException':
-                print("The request had invalid params:", e)
+                status_code = 500 
+                response_body = json.dumps(f"The request had invalid params: {e}")
             elif e.response['Error']['Code'] == 'DecryptionFailure':
-                print("The requested secret can't be decrypted using the provided KMS key:", e)
+                status_code = 500 
+                response_body = json.dumps(f"The requested secret can't be decrypted using the provided KMS key: {e}")
             elif e.response['Error']['Code'] == 'InternalServiceError':
-                print("An error occurred on service side:", e)
+                status_code = 500 
+                response_body = json.dumps(f"An error occurred on service side: {e}")
+            else:
+                status_code = 500 
+                response_body = json.dumps(f"Unknown error: {e}")
         else:
-            secret_value = response.get('SecretString', None)
-            secret_dict = json.loads(secret_value)
+            secret_value = json.loads(response.get('SecretString', None))
 
-        # Create dict of CyberArk metadata
-        cyberark_dict = {
-            "subdomain":    pcloud_secret_dict.get('subdomain', None),
-            "svc_username": pcloud_secret_dict.get('username', None),
-            "svc_password": pcloud_secret_dict.get('password', None),
-            "safe":         tags_dict.get('CyberArk Safe', None),
-            "account":      tags_dict.get('CyberArk Account', None),
-            "address":      secret_dict.get('address', None),
-            "username":     secret_dict.get('username', None),
-            "password":     secret_dict.get('password', None),
-            "platformId":   secret_dict.get('platformId', None)
-        }
 
-        # If no address, check if RDS Secret containing host
-        if cyberark_dict['address'] is None:
-            cyberark_dict['address'] = secret_dict.get('host', None)
-        
-        # If no platformId, check if RDS Secret contains as tag
-        if cyberark_dict['platformId'] is None:
-            cyberark_dict['platformId'] = tags_dict.get('CyberArk Platform', None)
+        if DEBUG:
+           print("getAsmSecretValue() ================")
+           print(f"\tsecret_id: {secret_id}\n\tsecret_value: {secret_value}")
+           print(f"\tstatus_code: {status_code}\n\tresponse: {response_body}")
 
-        print(f"CyberArk Metadata: {cyberark_dict}")
+        return secret_value, status_code, response_body
 
-        # Check for None as the value for any dict keys
-        none_keys = [key for key, value in cyberark_dict.items() if value is None]
+#-------------------------------------------
+def authnCyberArk(cyberark_dict):
+  # uses Pcloud creds in cyberark_dict to authenticate to CyberArk Identity
+  # returns session_token to use in further CyberArk API calls
 
-        if none_keys:
-            return {
-                'statusCode': 404,
-                'body': json.dumps(f"Required key value(s) not found: {none_keys}")
-            }
-            print(f'HTTP 404: Required key value(s) not found: {none_keys}')
+        session_token = None
+        status_code = 200
+        response_body = "Successfully authenticated to CyberArk Privilege Cloud."
 
         # Authenticate to CyberArk Identity
         url = f"https://{cyberark_dict['subdomain']}.cyberark.cloud/api/idadmin/oauth2/platformtoken"
-        payload = f"grant_type=client_credentials&client_id={urlify(cyberark_dict['svc_username'])}&client_secret={cyberark_dict['svc_password']}"
+        payload = f"grant_type=client_credentials&client_id={urlify(cyberark_dict['svc_username'])}&client_secret={urlify(cyberark_dict['svc_password'])}"
         headers = {
             'Content-Type': 'application/x-www-form-urlencoded'
         }
-        response = requests.request("POST", url, headers=headers, data=payload, timeout=30)
+        response = requests.request("POST", url, headers=headers, data=payload, timeout=60)
         if response.status_code == 200:
             # Parse the JSON response into a dictionary
             data = response.json()
-
-            # Access the session token from the dictionary
+            # Extract session token from the response dict
             session_token = data.get('access_token', None)
             if session_token is None:
-                return {
-                    'statusCode': 401,
-                    'body': json.dumps(f"There was a problem authenticating to: {cyberark_dict['subdomain']}.privilegecloud.cyberark.cloud")
-                }
+                status_code = 401
+                response_body = json.dumps(f"There was a problem authenticating to: {cyberark_dict['subdomain']}.privilegecloud.cyberark.cloud")
         else:
-            return {
-                'statusCode': 500,
-                'body': json.dumps(f"There was a problem authenticating to: {cyberark_dict['subdomain']}.privilegecloud.cyberark.cloud")
-            }
-        
+            status_code = 500,
+            response_body = json.dumps(f"There was a problem authenticating to: {cyberark_dict['subdomain']}.privilegecloud.cyberark.cloud")
+
+        if DEBUG:
+           print("authnCyberark() ================")
+           print(f"\tstatus_code: {status_code}\n\tresponse: {response_body}")
+
+        return session_token, status_code, response_body
+
+#-------------------------------------------
+def createSafe(cyberark_dict):
+  # Uses info in dictionary to create a safe in Privilege Cloude
+  # returns status_code == 201 on success & response_body message
+
+        status_code = 201
+        response_body = f"Safe {cyberark_dict['safe']} created successfully"
+
         # Create safe
         url = f"https://{cyberark_dict['subdomain']}.privilegecloud.cyberark.cloud/passwordvault/api/safes"
         payload = json.dumps({
@@ -152,17 +156,81 @@ def lambda_handler(event, context):
         }
         response = requests.request("POST", url, headers=headers, data=payload, timeout=30)
         print(f"Safe Response Message: {response.text}")
-        if response.status_code == 201:
-            print(f"Safe {cyberark_dict['safe']} created successfully")
-        elif response.status_code == 409:
-            print(f"Safe {cyberark_dict['safe']} already exists")
+        if response.status_code == 409:
+            status_code = 409
+            response_body = json.dumps(f"Safe {cyberark_dict['safe']} already exists")
         else:
-            return {
-                'statusCode': 500,
-                'body': json.dumps(f"There was a problem creating safe {cyberark_dict['safe']}")
-            }
+            status_code = 500
+            response_body = json.dumps(f"There was a problem creating safe {cyberark_dict['safe']}")
 
-        # Onboard account into safe
+        if DEBUG:
+           print("createSafe() ================")
+           print(f"\tstatus_code: {status_code}\n\tresponse: {response_body}")
+
+        return status_code, response_body
+
+#-------------------------------------------
+def assembleOnboardingDict(pcloud_secret_dict, tags_dict, secret_dict):
+  # 
+  # returns onboarding dictionary, status_code & response_body message
+
+        status_code = 200
+        response_body = "Onboarding dictionary assembled successfully."
+
+        # Construct dictionary of CyberArk metadata from pcloud, tags and secret dicts
+        cyberark_dict = {
+
+            # values pulled from service account secret
+            "subdomain":    pcloud_secret_dict.get('subdomain', None),
+            "svc_username": pcloud_secret_dict.get('username', None),
+            "svc_password": pcloud_secret_dict.get('password', None),
+
+            # values pulled from secret tags - standard CyberArk properties
+            "safe":         tags_dict.get('CyberArk Safe', None),
+            "account":      tags_dict.get('CyberArk Account', None),
+
+            # values pulled from RDS secret value - standard CyberArk properties
+            "address":      secret_dict.get('address', None),
+            "username":     secret_dict.get('username', None),
+            "password":     secret_dict.get('password', None),
+            "platformId":   secret_dict.get('platformId', None),
+            
+            # RDS values pulled from secret value - additional properties in secret
+            "dbInstanceIdentifier": secret_dict.get('dbInstanceIdentifier', None),
+            "host":                 secret_dict.get('host', None),
+            "engine":               secret_dict.get('engine', None),
+            "port":                 secret_dict.get('port', None)
+        }
+
+        # If no address, get secret value for 'host', if any
+        if cyberark_dict['address'] is None:
+            cyberark_dict['address'] = secret_dict.get('host', None)
+        
+        # If no platformId, get value for 'CyberArk Platform' tag, if any
+        if cyberark_dict['platformId'] is None:
+            cyberark_dict['platformId'] = tags_dict.get('CyberArk Platform', None)
+
+        # Validate no keys contain None as a value, if any, exit w/ 404
+        none_keys = [key for key, value in cyberark_dict.items() if value is None]
+        if none_keys:
+            status_code = 404
+            response_body =json.dumps(f"Required key value(s) not found: {none_keys}")
+
+        if DEBUG:
+           print("assembleOnboardingDict() ================")
+           print(f"\ncyberark_dict: {cyberark_dict}")
+           print(f"\tstatus_code: {status_code}\n\tresponse: {response_body}")
+
+        return cyberark_dict, status_code, response_body
+
+#-------------------------------------------
+def onboardAccount(cyberark_dict, session_token):
+  # uses values in cyberark_dict to create account
+  # returns status_code == 201 for success, response_body with message
+
+        status_code = 201
+        response_body = json.dumps(f"Account {cyberark_dict['account']} onboarded successfully")
+
         url = f"https://{cyberark_dict['subdomain']}.privilegecloud.cyberark.cloud/passwordvault/api/accounts"
         payload = json.dumps({
             "safeName": cyberark_dict['safe'],
@@ -174,6 +242,12 @@ def lambda_handler(event, context):
             "secret": cyberark_dict['password'],
             "secretManagement": {
                 "automaticManagementEnabled": True
+            },
+            "platformAccountProperties": {
+                "port": cyberark_dict['port'],
+                "host": cyberark_dict['host'],
+                "dbInstanceIdentifier": cyberark_dict['dbInstanceIdentifier'],
+                "engine": cyberark_dict['engine']
             }
         })
         headers = {
@@ -181,21 +255,75 @@ def lambda_handler(event, context):
             'Authorization': f'Bearer {session_token}'
         }
         response = requests.request("POST", url, headers=headers, data=payload, timeout=30)
-        print(f"Account Response Message: {response.text}")
-        if response.status_code == 201:
-            print(f"Account {cyberark_dict['account']} onboarded successfully")
-        elif response.status_code == 409:
-            print(f"Account {cyberark_dict['account']} already exists")
+        if response.status_code == 409:
+            status_code = 409
+            response_body = json.dumps(f"Account {cyberark_dict['account']} already exists")
         else:
-            return {
-                'statusCode': 500,
-                'body': json.dumps(f"There was a problem onboarding account {cyberark_dict['account']}")
+            status_code = 500
+            response_body = json.dumps(f"{response.text}")
+
+        if DEBUG:
+           print("onboardAccount() ================")
+           print(f"\turl: {url}\n\tpayload: {payload}")
+           print(f"\tstatus_code: {status_code}\n\tresponse: {response_body}")
+
+        return status_code, response_body
+
+#############################################
+#############################################
+# Lambda function handler (main entrypoint)
+
+def lambda_handler(event, context):
+    prologOut(event, context)
+
+    try:
+      # Extract the ID of secret to onboard from the triggering event
+        secret_id = event['detail']['requestParameters']['name']
+
+      # Initialize a client for AWS Secrets Manager
+        secrets_manager_client = boto3.client('secretsmanager')
+
+      # Validate secret is correctly tagged for onboarding
+        tags_dict, status_code, response_body = validateSecretTags(secrets_manager_client, secret_id)
+        if status_code != 200:
+          return { 'statusCode': status_code, 'body': response_body }
+
+      # Retrieve the id of secret holding admin creds from env var
+        pcloud_secret_id = os.environ.get(PCLOUD_ADMIN_SECRET_ENV_VAR, None)
+        if pcloud_secret_id is None:
+            return { 'statusCode': 404,
+                     'body': f"Env var '{PCLOUD_ADMIN_SECRET_ENV_VAR}' not found in lambda environment variables." 
             }
 
-        return {
-            'statusCode': 200,
-            'body': json.dumps(f"Tags retrieved for secret {secret_id}: {tags}")
-        }
+      # Get Pcloud admin creds from ASM
+        pcloud_secret_dict, status_code, response_body = getAsmSecretValue(secrets_manager_client, pcloud_secret_id)
+        if status_code != 200:
+          return { 'statusCode': status_code, 'body': response_body }
+
+      # Retrieve the secret value of the secret
+        secret_dict, status_code, response_body = getAsmSecretValue(secrets_manager_client, secret_id)
+        if status_code != 200:
+          return { 'statusCode': status_code, 'body': response_body }
+
+      # Assemble all info into a single onboarding dictionary
+        onboarding_dict, status_code, response_body = assembleOnboardingDict(pcloud_secret_dict, tags_dict, secret_dict)
+        if status_code != 200:
+          return { 'statusCode': status_code, 'body': response_body }
+
+      # Authenticate to Privilege Cloud
+        session_token, status_code, response_body  = authnCyberArk(onboarding_dict)
+        if status_code != 200:
+           return { 'statusCode': status_code, 'body': response_body }
+
+      # Create safe in Privilege Cloud
+#       status_code, response_body = createSafe(onboarding_dict)
+#        if status_code != 201:
+#           return { 'statusCode': status_code, 'body': response_body }
+
+      # Onboard account into safe
+        status_code, response_body = onboardAccount(onboarding_dict, session_token)
+        if status_code != 201:
+           return { 'statusCode': status_code, 'body': response_body }
 
     except ClientError as e:
         print(f"An error occurred: {e}")
@@ -203,3 +331,5 @@ def lambda_handler(event, context):
             'statusCode': 500,
             'body': json.dumps(f"Error retrieving tags for secret: {e}")
         }
+
+    return { 'statusCode': 200, 'body': f"Secret {secret_id} onboarded successfully." }
